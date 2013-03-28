f8cfb7955d7a953e664e4bafcb47eca709e6cbc4
unknown <omar.abdelkader.92@gmail.com>
2013-03-28 03:27:35 -0400

    Alien Moves and Shoots and detects collision with bullets

--- src/astr_pkg/Alien.java
+++ src/astr_pkg/Alien.java
@@ -5,129 +5,139 @@ import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 import javax.swing.ImageIcon;
 
-public class Alien implements Runnable {
+public class Alien {
     
     private static Image AlienImage;
-    double xCoor, yCoor, AIx, AIy;
-    int xDIR, yDIR;
+    double shipX, shipY, xPos, yPos;
+    int xVelocity, yVelocity;
     private static Alien[] aliens;
     private static int drawDelay = 25;
     private static int alienCount = 1;
-    private ArrayList<Projectiles> projectiles;
+    private ArrayList<ProjectilesAliens> projectiles;
     double shoot;
     private Ellipse2D.Double circle;
-    private double height;
-    private double width;
+    private double alienHeight;
+    private double alienWidth;
+    private static int pointsPlayer1 = 0;
+    private static int numberOfAliens;
     
-    public Alien(double A, double B, double shipXPos, double shipYPos){
-        AIx = A;
-        AIy = B;
-        xCoor = shipXPos;
-        yCoor = shipYPos;
+    private static int[] scoreX;
+	private static int[] scoreY;
+	private static int[] scoreTime;
+	private static final int scoreValue = 100;
+    
+    public Alien(double alienXPos, double alienYPos, double shipXPos, double shipYPos){
+        xPos = alienXPos;
+        yPos = alienYPos;
+        shipX = shipXPos;
+        shipY = shipYPos;
         AlienImage = new ImageIcon("src/astr_pkg/Alien.png").getImage();
-        height = AlienImage.getHeight(null);
-        width = AlienImage.getWidth(null);
-        projectiles = new ArrayList<Projectiles>();
+        alienHeight = AlienImage.getHeight(null);
+        alienWidth = AlienImage.getWidth(null);
+        projectiles = new ArrayList<ProjectilesAliens>();
     }
     
-    public static void generateAliens(int count){
-        aliens = new Alien[count];
+    public static void generateAliens(int numAliens){
+    	numberOfAliens = numAliens;
+        aliens = new Alien[numAliens];
         double alienX, alienY;
-        for(int i = 0; i < count; i++){
+        for(int i = 0; i < numAliens; i++){
             alienX =  (Math.ceil(Math.random() * 500));
             alienY =  (Math.ceil(Math.random() * 500));
             aliens[i] = new Alien(alienX, alienY, Constants.SHIP.getX(), Constants.SHIP.getY());
         }
+        scoreX = new int[numAliens];
+		scoreY = new int[numAliens];
+		scoreTime = new int[numAliens];
     }
     
     public static Alien[] getAliens(){
         return aliens;
     }
     
-    public ArrayList<Projectiles> getShots(){
+    public ArrayList<ProjectilesAliens> getShots(){
         return this.projectiles;
     }
     
+    public static int getPointsP1(){
+    	return pointsPlayer1;
+    }
+    
+    public static int getNumAliens(){
+    	return numberOfAliens;
+    }
+    
     public static void drawAlien(Graphics g){
-        for(int i = 0; i < alienCount; i++){
-            g.drawImage(AlienImage, (int)aliens[i].AIx, (int)aliens[i].AIy, null);
+        for(int i = 0; i < aliens.length; i++){
+        	if(aliens[i] != null){
+        		g.drawImage(AlienImage, (int)aliens[i].xPos, (int)aliens[i].yPos, null);
+        		aliens[i].checkCollisionProjectile();
+        	}  
         }
         if(drawDelay < 0 && alienCount < aliens.length){
             alienCount++;
-            drawDelay = 125;
+            drawDelay = 45*40;
         }else{
             drawDelay--;
         }
-        Graphics2D g2D = (Graphics2D) g;
-//        for(int i = 0; i < aliens.length; i++){
-//            if(aliens[i].collisionProjectile() || aliens[i].collisionShip()){
-//                g.setColor(Color.red);
-//                g2D.draw(aliens[i].circle);
+        for(int i = 0; i < scoreX.length; i++){
+			if(scoreX[i] != 0 || scoreY[i] != 0){ //There is a score
+				if(scoreTime[i] > 0){
+					g.setFont(new Font("Times-Roman", Font.BOLD, 12));
+					g.setColor(Color.RED);
+					g.drawString("" + scoreValue, scoreX[i], scoreY[i]);
+					scoreTime[i]--;
+					scoreY[i] -= 1;
+				}
+			}
+		}
+    }
+    
+    public void find(){
+    	double alienDistance = 
+    			Math.sqrt(Math.pow((shipX - xPos), 2) + 
+    					Math.pow((shipY - yPos), 2));
+    	
+    	if(alienDistance < 100){
+    		changeX(0);
+    		changeY(0);
+    	}else{
+    		int randomValueX = (int) (Math.random() * 10) % 5;
+    		int randomValueY = (int) (Math.random() * 10) % 5;
+    		int xVel = xPos < shipX ? randomValueX : -randomValueX;
+    		int yVel = yPos < shipY ? randomValueY : -randomValueY;
+    		changeX(xVel);
+    		changeY(yVel);
+    	}
+//        if((Math.abs(shipX-xPos)) < 32){
+//            changeX(0);
+//        }else{
+//            if(xPos < shipX){
+//                changeX(1);
+//            }
+//            if(xPos > shipX){
+//                changeX(-1);
+//            }
+//        }
+//        if((Math.abs(shipY-yPos)) < 32){
+//            changeY(0);
+//        }else{
+//            if(yPos < shipY){
+//                changeY(1);
+//            }
+//            if(yPos > shipY){
+//                changeY(-1);
 //            }
 //        }
     }
     
-    public void find(){
-        
-        /*if((Math.abs(xCoor-AIx)) < 0.5){
-         int random = (int) (Math.random() * 8);
-         if(random <= 2){
-         changeX(25);
-         changeY(25);
-         }else if(random > 2 && random <= 4){
-         changeX(25);
-         changeY(575);
-         }else if(random > 4 && random <= 6){
-         changeX(775);
-         changeY(25);
-         }else{
-         changeX(775);
-         changeY(575);
-         }
-         }else{*/
-        if((Math.abs(xCoor-AIx)) < 32){
-            changeX(0);
-        }else{
-            if(AIx < xCoor){
-                changeX(1);
-            }
-            if(AIx > xCoor){
-                changeX(-1);
-            }
-        }
-        /*if((Math.abs(yCoor-AIy)) < 0.5){
-         System.out.println(yCoor-AIy);
-         int random = (int) (Math.random() * 8);
-         if(random <= 2){
-         changeX(25);
-         changeY(25);
-         }else if(random > 2 && random <= 4){
-         changeX(25);
-         changeY(575);
-         }else if(random > 4 && random <= 6){
-         changeX(775);
-         changeY(25);
-         }else{
-         changeX(775);
-         changeY(575);
-         }
-         }else{*/
-        if((Math.abs(yCoor-AIy)) < 32){
-            changeY(0);
-        }else{
-            if(AIy < yCoor){
-                changeY(1);
-            }
-            if(AIy > yCoor){
-                changeY(-1);
-            }
-        }
-    }
-    
     public void shoot(){
         if(shoot >= 50){
-            double theta = 120; //need function
-            Projectiles AlienProjectiles = new Projectiles(AIx, AIy, theta);
+        	double deltaX = shipX - xPos;
+        	double deltaY = shipY - yPos;
+            double theta = Math.atan2(deltaY, deltaX); //need function
+            ProjectilesAliens AlienProjectiles = 
+            		new ProjectilesAliens(xPos, yPos, theta, xVelocity, yVelocity);
             projectiles.add(AlienProjectiles);
             shoot = 0;
         }else{
@@ -136,33 +146,33 @@ public class Alien implements Runnable {
     }
     
     public void detectEdges(){
-        if(AIx <= 16){
+        if(xPos <= 16){
             changeX(1);
         }
-        if(AIx >= 784){
+        if(xPos >= 784){
             changeX(-1);
         }
-        if(AIy <= 16){
+        if(yPos <= 16){
             changeY(1);
         }
-        if(AIy >= 584){
+        if(yPos >= 584){
             changeY(-1);
         }
     }
     
     public void changeX(int dir){
-        xDIR = dir;
+        xVelocity = dir;
     }
     
     public void changeY(int dir){
-        yDIR = dir;
+        yVelocity = dir;
     }
     
     public void move(){
-        AIx += xDIR;
-        AIy += yDIR;
+        xPos += xVelocity;
+        yPos += yVelocity;
         
-        circle = new Ellipse2D.Double(AIx, AIy, width, height);
+        circle = new Ellipse2D.Double(xPos, yPos, alienWidth, alienHeight);
     }
     
     public Ellipse2D getBounds(){
@@ -173,27 +183,21 @@ public class Alien implements Runnable {
     	return circle.intersects(Constants.SHIP.getBounds());
     }
     
-    public boolean collisionProjectile(){
+    public void checkCollisionProjectile(){
         for(int i = 0; i < Constants.SHIP.getProjectiles().size(); i++){
     		if(circle.intersects(Constants.SHIP.getProjectiles().get(i).getProjectileBounds())){
 				Constants.SHIP.getProjectiles().remove(i);
-				return true;
+				for(int j = 0; j < aliens.length; j++){
+					if(aliens[j] != null && aliens[j].equals(this)){
+						scoreX[j] = (int) this.xPos;
+						scoreY[j] = (int) this.yPos;
+						scoreTime[j] = 160;
+						aliens[j] = null;
+						numberOfAliens--;
+						pointsPlayer1 += scoreValue;
+					}
+				}
             }
     	}
-    	return false;
-    }
-    
-    public void run(){
-        try{
-            while(true){
-                shoot();
-                find();
-                move();
-                detectEdges();
-                Thread.sleep(5);
-            }
-        }catch(Exception ex){
-            System.err.println(ex.getMessage());
-        }
     }
 }
